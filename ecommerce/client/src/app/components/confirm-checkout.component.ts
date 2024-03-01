1be5a4970d75e5399b1fcf7a879079b06daa62e7
Isaac Tee <iztyj.work@gmail.com>
2024-03-01 11:31:27 +0800

    updated task 3

--- ecommerce/client/src/app/components/confirm-checkout.component.ts
+++ ecommerce/client/src/app/components/confirm-checkout.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CartStore } from '../cart.store';
+import { LineItem } from '../models';
 
 @Component({
   selector: 'app-confirm-checkout',
   templateUrl: './confirm-checkout.component.html',
   styleUrl: './confirm-checkout.component.css'
 })
-export class ConfirmCheckoutComponent {
+export class ConfirmCheckoutComponent implements OnInit{
 
   // TODO Task 3
+  private fb = inject(FormBuilder)
+  private cartStore = inject(CartStore)
+
+  checkoutForm !: FormGroup
+  cart!:LineItem[]
+  total:number=0
+
+  ngOnInit(): void {
+      this.checkoutForm = this.createForm()
+      this.subscribeToCartStore()
+      this.calculateTotal()
+  }
+
+  createForm(){
+    return this.fb.group({
+      name: this.fb.control<string>('', Validators.required),
+      address : this.fb.control<string>('',[Validators.required,Validators.minLength(3)]),
+      priority : this.fb.control<boolean>(false),
+      comments : this.fb.control<string>('')
+    })
+  }
+
+  subscribeToCartStore(){
+    this.cartStore.getItems.subscribe({
+      next:(data)=>{
+        this.cart = data
+      }
+    })
+  }
+
+  calculateTotal(){
+    // var total:number = 0
+    for(let i of this.cart){
+      const price = i.price
+      const qty = i.quantity
+      const product = price*qty
+      this.total+=product
+    }
+  }
 
 }
